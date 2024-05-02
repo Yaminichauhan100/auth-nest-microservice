a6eccfa029a544257e99f9c63ef2162aacd6b430
Yamini20000 <ext-ych@ecco.com>
2024-05-02 13:20:25 +0530

    code refactoring

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello up and running on port 3002!';
+    return 'Hello up and running on port 3003!';
   }
 }
